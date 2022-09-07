a4e4a6c3ca4aac055007b6667bbffaf0234c723d
MoDasby <giullianomendes033@gmail.com>
2022-09-07 15:49:30 +0000

    add title to feed component

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,10 +1,14 @@
 import {Post, PostProps} from "../PostCard";
 import {PostInput} from "../PostInput";
+import './style.css';
 
 export const Feed = ({ props }: { props: PostProps[] }) => {
     return (
         <>
             <PostInput />
+
+            <h1 className="feed-title">Seu Feed</h1>
+
             {
                 props.map((p, index) => (<Post props={p} key={index} />))
             }
