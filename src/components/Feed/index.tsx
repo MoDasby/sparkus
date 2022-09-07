7694c7459a596839dea70adb89740006d1232a16
MoDasby <giullianomendes033@gmail.com>
2022-09-07 16:04:32 +0000

    add post input to feed component

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,5 +1,13 @@
-export const Feed = ({ posts }) => {
+import {Post, PostProps} from "../PostCard";
+import {PostInput} from "../PostInput";
+
+export const Feed = ({ props }: { props: PostProps[] }) => {
     return (
-        <></>
+        <>
+            <PostInput />
+            {
+                props.map((p, index) => (<Post props={p} key={index} />))
+            }
+        </>
     )
 }
