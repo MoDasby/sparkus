c9e48a4937580fbc5172f56e14c7ecf458b1fa4e
MoDasby <giullianomendes033@gmail.com>
2022-09-10 19:38:34 +0000

    router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,15 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./Pages/HomePage";
 import Search from "./Pages/SearchPage";
+import {ProfileDetailsPage} from "./Pages/ProfileDetailsPage";
 
 export const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/search" element={<Search />} />
+                <Route path="/" element={ <Home /> } />
+                <Route path="/search" element={ <Search /> } />
+                <Route path="/user" element={ <ProfileDetailsPage /> } />
             </Routes>
         </BrowserRouter>
     )
