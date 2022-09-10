e248abbf41ce705dc501dd7e03910b689c023cb7
MoDasby <giullianomendes033@gmail.com>
2022-09-10 19:36:45 +0000

    bug correction

--- src/components/PostInput/index.tsx
+++ src/components/PostInput/index.tsx
@@ -7,12 +7,13 @@ export const PostInput = () => {
                 <img
                     className="profile-picture"
                     src="https://cdn.pocket-lint.com/r/s/1200x630/assets/images/162179-tv-news-feature-rick-and-morty-season-6-release-date-trailer-and-how-to-watch-image1-kbmgzwpsy5.jpg"
+                    alt="foto do perfil"
                 />
             </div>
             <form className="post-input-form">
                 <textarea placeholder="No que está pensando?" className="post-input" />
                 <button
-                    className="post-input-submit"
+                    className="btn"
                     type="submit"
                     onClick={(e) => e.preventDefault()}
                 >
